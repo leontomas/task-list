183f87c08bf677cb9034473a0fd57568afcf8bb2
Leonardo Tomas Jr <leonardotomasjr@gmail.com>
2024-02-20 08:32:42 +0800

    added routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*Route::get('/', function () {
+    return 'view('welcome')';
+});*/
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
 });
+
+Route::get('/xxx', function () {
+    return 'Hello';
+})->name('hello');
+
+// redirect
+Route::get('/hallo', function () {
+    return redirect('')->route('hello');
+});
+
+// dynamic url
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
+});
+// fallback
+Route::fallback(function () {
+    return 'Still got somewhere!';
+});
+
+// GET
+// POST
+// PUT
+// DELETE
